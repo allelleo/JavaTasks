0540f6caa15446d33ae3821733f0c993ec7a7d72
Alexey Ovchinnikov <119781637+allelleo@users.noreply.github.com>
2024-10-18 15:05:37 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/second/task4.java
+++
@@ -1,9 +0,0 @@
-package org.example.second;
-
-public class task4 {
-    public static void checkAge(int age) {
-        if (age < 0 || age > 150) {
-            throw new IllegalArgumentException("Возраст должен быть в диапазоне от 0 до 150");
-        }
-    }
-}
